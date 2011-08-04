1835ccca4544a62ec0a33f2ca26d3ed6d4bab421
S.Nishio <nishio@densan-labs.net>
2011-08-04 15:40:55 +0900

    設定画面更新

---
+++ app/controllers/add_link_controller.rb
@@ -0,0 +1,20 @@
+class AddLinkController < ApplicationController
+  unloadable
+
+  def add_index
+  end
+
+  def add
+    replace_string = params[:replace_string]
+    replace_url = params[:replace_url]
+    
+    if replace_string && replace_url
+      data = UrlStores.new
+      data.replace_string = replace_string
+      data.replace_url = replace_url
+      data.save
+      @success = true
+    end
+    
+  end
+end
