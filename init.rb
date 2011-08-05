6934c0d814a8839dcc18936124a7cf8e6b4bd097
S.Nishio <nishio@densan-labs.net>
2011-08-05 16:55:47 +0900

    任意のURLへのリンクが簡単に作成できるようになった

--- init.rb
+++ init.rb
@@ -17,7 +17,10 @@ class LinkConvertHelper
     url = @@url_list[key]
     if url
       url.strip!
-      return url + value
+      value.length.times do |i|
+        url.gsub!( "\[#{i}\]", value[i].strip)
+      end
+      return url
     end
   end
 
@@ -25,32 +28,33 @@ class LinkConvertHelper
     if args.size < 2
       return nil
     end
-    
     link_key = args[0].to_s.strip
-    link_value = args[1].to_s.strip
-
+    link_value = args
     tag = get_url(link_key, link_value)
+
     if tag
-      return "<a href=#{tag}>#{link_key} #{link_value}</a>"
+      link = "<a href=#{tag}> "
+      link_value.each do | d |
+        link = link + d.strip + " "
+      end
+      link = link + "</a>"
+      return link
     end
     return "Error: Link not found"
   end
 end
 
 Redmine::Plugin.register :redmine_wikilink_converter do
-  name 'Redmine Eviden Wikilink Converter plugin'
+  name 'Redmine Wikilink Converter plugin'
   author 'Shinsuke Nishio'
-  description 'evidenへのリンクを作成するwikiプラグイン'
+  description 'wiki内でリンクを簡単に作成するためのプラグイン'
   version '0.0.1'
 
-  settings :default => {
-    'replace_string' => 'key',
-    'replace_address' => 'testurl'
-  }, :partial => 'settings/eviden_settings'
+  settings :partial => 'settings/wikilink_settings'
   
   Redmine::WikiFormatting::Macros.register do
     desc "nothing"
-    macro :eviden do |obj, args|
+    macro :wikilink do |obj, args|
       h = LinkConvertHelper.new
       h.get_tag(args)
     end
